7dc9c0229e63b50e6220b2b2519f7e33d6b88228
soyjesusdp <jesusduertoprado@gmail.com>
2024-11-30 11:58:47 -0600

    Comments added

--- src/components/Pages/Principalpage/Principalpage.jsx
+++ src/components/Pages/Principalpage/Principalpage.jsx
@@ -3,22 +3,27 @@ import "./Principalpage.css";
 
 const Principalpage = () => {
     return (
-        <div>
+        <div> {/*DIV DE INICIO*/}
+
+        
             {/*IMAGEN GRANDE DEL INICIO*/}
-        <div className="MainContainer">
-            <img className="background-image" src="./Imagenes/Pozole.png" alt="Olla de pozole rojo"/>
-            {/*BOTONES ENCIMA DE LA IMAGEN GRANDE DEL INICIO*/}
-            <div className="MainContainerText">
-                <h1 className="h1MCT">Platillos exquisitos</h1>
-                <h2 className="h2MCT">TODOS LOS DÍAS</h2>
+            <div className="MainContainer">
+                <img className="background-image" src="./Imagenes/Pozole.png" alt="Olla de pozole rojo" />
+                {/*BOTONES ENCIMA DE LA IMAGEN GRANDE DEL INICIO*/}
+                <div className="MainContainerText">
+                    <h1 className="h1MCT">Platillos exquisitos</h1>
+                    <h2 className="h2MCT">TODOS LOS DÍAS</h2>
+                </div>
+                {/*BOTONES ENCIMA DE LA IMAGEN GRANDE DEL INICIO*/}
+                <div className="MainContainerButtons">
+                    <a href="#" className="button"><strong>Suscríbete</strong></a>
+                    <a href="#" className="button"><strong>Menú a domicilio</strong></a>
+                </div>
             </div>
-            {/*BOTONES ENCIMA DE LA IMAGEN GRANDE DEL INICIO*/}
-            <div className="MainContainerButtons">
-                <a href="#" className="button"><strong>Suscríbete</strong></a>
-                <a href="#" className="button"><strong>Menú a domicilio</strong></a>
-            </div>
-        </div>
-        </div>
+
+
+        {/*DIV DE CIERRE*/}
+        </div> 
     );
 };
 
