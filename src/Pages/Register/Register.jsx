5928eee0af59d333f731cf2df8076f44ad391982
JhoselPR <112789135+JhoselPR@users.noreply.github.com>
2024-12-17 15:32:38 -0600

    Scroll Sign up and Login

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -29,7 +29,9 @@ const Card = styled(MuiCard)({
     gap: '1.6rem',
     margin: 'auto',
     marginTop: '3rem',
-    borderRadius: '10px',
+    borderRadius: '20px',
+    overflowY: 'auto',
+    overflowX: 'hidden',
     boxShadow:
         'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
     '@media (min-width: 600px)': {
