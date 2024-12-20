c7f762863aab83573207de42bbb25c88ee3fbbcf
soyjesusdp <jesusduertoprado@gmail.com>
2024-12-20 10:27:54 -0600

    Corazones quitados

--- src/Pages/Principalpage/Principalpage.jsx
+++ src/Pages/Principalpage/Principalpage.jsx
@@ -97,8 +97,6 @@ const Testimonials = () => (
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         />
-
-        <CustomizedRating />
         <span className="fa fa-star checked"></span>
         <span className="fa fa-star checked"></span>
         <span className="fa fa-star checked"></span>
